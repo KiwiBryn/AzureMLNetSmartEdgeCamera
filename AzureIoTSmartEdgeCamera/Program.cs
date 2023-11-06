1f5465a665a53534c819c60e0f6953874c37c009
Bryn Lewis <bryn.lewis@devmobile.co.nz>
2023-11-06 19:06:12 +1300

    Discovered than fixed security camera async issue

--- AzureIoTSmartEdgeCamera/Program.cs
+++ AzureIoTSmartEdgeCamera/Program.cs
@@ -58,14 +58,19 @@ namespace devMobile.IoT.MachineLearning.AzureIoTSmartEdgeCamera
 #endif
    using Microsoft.Extensions.Configuration;
 
+#if AZURE_IOT_HUB_CONNECTION || AZURE_IOT_HUB_DPS_CONNECTION
    using Newtonsoft.Json;
    using Newtonsoft.Json.Linq;
+#endif
 
    using SixLabors.ImageSharp.PixelFormats;
    using SixLabors.ImageSharp;
+
+#if OUTPUT_IMAGE_MARKUP
    using SixLabors.ImageSharp.Processing;
    using SixLabors.ImageSharp.Drawing.Processing;
    using SixLabors.Fonts;
+#endif
 
    using Yolov5Net.Scorer;
    using Yolov5Net.Scorer.Models;
@@ -248,7 +253,7 @@ namespace devMobile.IoT.MachineLearning.AzureIoTSmartEdgeCamera
          try
          {
 #if SECURITY_CAMERA
-            SecurityCameraImageCapture();
+            await SecurityCameraImageCaptureAsync();
 #endif
 
 #if RASPBERRY_PI_CAMERA
@@ -268,7 +273,27 @@ namespace devMobile.IoT.MachineLearning.AzureIoTSmartEdgeCamera
                predictions = _scorer.Predict(image);
                Console.WriteLine($" {DateTime.UtcNow:yy-MM-dd HH:mm:ss:fff} YoloV5 inferencing done");
 
-               OutputImageMarkup(image, predictions);
+#if OUTPUT_IMAGE_MARKUP
+               var font = new Font(new FontCollection().Add(_applicationSettings.ImageMarkUpFontPath), _applicationSettings.ImageMarkUpFontSize);
+
+               foreach (var prediction in predictions)
+               {
+                  double score = Math.Round(prediction.Score, 2);
+
+                  var (x, y) = (prediction.Rectangle.Left - 3, prediction.Rectangle.Top - 23);
+
+                  image.Mutate(a => a.DrawPolygon(Pens.Solid(prediction.Label.Color, 1),
+                      new PointF(prediction.Rectangle.Left, prediction.Rectangle.Top),
+                      new PointF(prediction.Rectangle.Right, prediction.Rectangle.Top),
+                      new PointF(prediction.Rectangle.Right, prediction.Rectangle.Bottom),
+                      new PointF(prediction.Rectangle.Left, prediction.Rectangle.Bottom)
+                  ));
+
+                  image.Mutate(a => a.DrawText($"{prediction.Label.Name} ({score})", font, prediction.Label.Color, new PointF(x, y)));
+               }
+
+               image.Save(_applicationSettings.ImageOutputFilenameLocal);
+#endif
             }
 
 #if PREDICTION_CLASSES_OF_INTEREST
@@ -318,7 +343,7 @@ namespace devMobile.IoT.MachineLearning.AzureIoTSmartEdgeCamera
       }
 
 #if SECURITY_CAMERA
-      private static async void SecurityCameraImageCapture()
+      private static async Task SecurityCameraImageCaptureAsync()
       {
          Console.WriteLine($" {DateTime.UtcNow:yy-MM-dd HH:mm:ss:fff} Security Camera Image download start");
 
@@ -408,33 +433,6 @@ namespace devMobile.IoT.MachineLearning.AzureIoTSmartEdgeCamera
          Console.WriteLine($" {DateTime.UtcNow:yy-MM-dd HH:mm:ss} Azure IoT Hub DPS connection done");
 
          return deviceClient;
-      }
-#endif
-
-#if OUTPUT_IMAGE_MARKUP
-      public static void OutputImageMarkup(Image<Rgba32> image, List<YoloPrediction> predictions)
-      {
-         Console.WriteLine($" {DateTime.UtcNow:yy-MM-dd HH:mm:ss:fff} Image markup start");
-
-         var font = new Font(new FontCollection().Add(_applicationSettings.ImageMarkUpFontPath), _applicationSettings.ImageMarkUpFontSize);
-
-         foreach (var prediction in predictions)
-         {
-            double score = Math.Round(prediction.Score, 2);
-
-            var (x, y) = (prediction.Rectangle.Left - 3, prediction.Rectangle.Top - 23);
-
-            image.Mutate(a => a.DrawPolygon(Pens.Solid(prediction.Label.Color, 1),
-                new PointF(prediction.Rectangle.Left, prediction.Rectangle.Top),
-                new PointF(prediction.Rectangle.Right, prediction.Rectangle.Top),
-                new PointF(prediction.Rectangle.Right, prediction.Rectangle.Bottom),
-                new PointF(prediction.Rectangle.Left, prediction.Rectangle.Bottom)
-            ));
-
-            image.Mutate(a => a.DrawText($"{prediction.Label.Name} ({score})", font, prediction.Label.Color, new PointF(x, y)));
-         }
-
-         Console.WriteLine($" {DateTime.UtcNow:yy-MM-dd HH:mm:ss:fff} Image markup done");
       }
 #endif
 
